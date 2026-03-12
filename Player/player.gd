aeaba8fefdc60d7bc4f6084b7f878e15e23d4a06
Pieter de jong <144777570+Pietlander@users.noreply.github.com>
2026-03-12 16:28:22 +0100

    Player animation
    Added player animations

--- Player/player.gd
+++ Player/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-# Tweak these numbers until it feels perfect for your game
-const MAX_SPEED = 100.0
+
+const MAX_SPEED = 400.0
 const ACCELERATION = 600.0
 const FRICTION = 800.0
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	if direction != Vector2.ZERO:
-		# If the player is pressing a key, accelerate towards the max speed
 		velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+		animated_sprite.play("Running")
 	else:
-		# If no keys are pressed, apply friction to slide to a stop
+		animated_sprite.play("idel")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	move_and_slide()
